2dda5cd067067a7d1b3569424bdb197e7244f233
Nishu0 <89217455+Nishu0@users.noreply.github.com>
2021-08-21 22:52:44 +0530

    Practical6

---
+++ 6.2.c
@@ -0,0 +1,19 @@
+/* 
+Problem Statement::Write a program to print the multiplication table of the number entered from
+the keyboard. The table should get displayed in the following form:
+12*1=12
+12*2=24
+….
+*/
+
+#include <stdio.h>
+
+int main() {
+  int n, i;
+  printf("Enter an integer:: ");
+  scanf("%d", & n);
+  for (i = 1; i <= 10; ++i) {
+    printf("%d x %d = %d \n", n, i, n * i);
+  }
+  return 0;
+}
