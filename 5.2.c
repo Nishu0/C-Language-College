e3337472f815260f706a1f840b9cb7ec64e62817
Nishu0 <89217455+Nishu0@users.noreply.github.com>
2021-08-21 22:48:32 +0530

    Practical5

---
+++ 5.2.c
@@ -0,0 +1,38 @@
+/* 
+Problem Statement::If the ages of Ram, Shyam and Ajay are input through the keyboard, write a
+program to determine the youngest of the three. (Hint: Use Nested Switch Statement)
+*/
+
+#include<stdio.h>
+
+int main() {
+  int r, s, a;
+  int i = 1;
+  printf("Enter the age of Ram:: ");
+  scanf("%d", & r);
+  printf("Enter the name of Shyam:: ");
+  scanf("%d", & s);
+  printf("Enter the name of Ajay:: ");
+  scanf("%d", & a);
+  switch (i) {
+  case 1:
+    if (r <= s && r <= a) {
+      printf("Ram is yongest");
+      i = 2;
+    }
+    switch (i) {
+    case 1:
+      if (s <= a && s <= r) {
+        printf("Shyam is youngest");
+        i = 2;
+      }
+      switch (i) {
+      case 1:
+        if (a <= s && a <= r) {
+          printf("Ajay is the youngest\n");
+        }
+      }
+    }
+  }
+  return 0;
+}
