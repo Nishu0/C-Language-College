5592653374613114d5a7d731a2790130682f9d9d
Nishu0 <89217455+Nishu0@users.noreply.github.com>
2021-08-21 22:31:57 +0530

    Practical3

---
+++ 3.1.c
@@ -0,0 +1,21 @@
+/* 
+Problem Statement::Write a program to find the greatest of the three numbers entered through
+the keyboard using conditional operators.
+*/
+
+#include <stdio.h>
+
+int main() {
+        int n1, n2, n3;
+        printf("Enter three numbers: ");
+        scanf("%d %d %d", & n1, & n2, & n3);
+
+        if (n1 >= n2 && n1 >= n3)
+                printf("%d is the largest number.", n1);
+        else if (n2 >= n1 && n2 >= n3)
+                printf("%d is the largest number.", n2);
+        else
+                printf("%d is the largest number.", n3);
+
+        return 0;
+}
